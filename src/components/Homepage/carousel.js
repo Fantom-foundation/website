6c010118ac1917cc84d2fa4bd3b5f4858b898e25
alka95 <deepujha1112@gmail.com>
2019-11-18 22:09:42 +0530

    date issue fixed

--- src/components/Homepage/carousel.js
+++ src/components/Homepage/carousel.js
@@ -2,6 +2,18 @@ import React from "react"
 import Slider from "react-slick"
 import axios from "axios"
 
+function formatDate(newDate) {
+  const monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+  let date = new Date(newDate);
+
+  return monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+}
+
 class Carousel extends React.Component {
   constructor(){
     super();
@@ -21,9 +33,9 @@ class Carousel extends React.Component {
     })
   }
   render() {
-let postsState = this.state.posts
-console.log('postsState ', postsState);
-console.log('postsState type', typeof postsState);
+    let postsState = this.state.posts
+    console.log('postsState ', postsState);
+    console.log('postsState type', typeof postsState);
     var settings = {
       arrows: true,
       infinite: true,
@@ -42,29 +54,13 @@ console.log('postsState type', typeof postsState);
             postsState.map((post, index) => {
               var StrippedString = post.content.split(" ").splice(0, 20).join(" ");
               var StrippedText = StrippedString.replace(/(<([^>]+)>)/ig,"");
-              var newDate = post.pubDate;
-              function formatDate(newDate) {
-                var monthNames = [
-                  "January", "February", "March",
-                  "April", "May", "June", "July",
-                  "August", "September", "October",
-                  "November", "December"
-                ];
-              
-                var day = newDate.getDate();
-                var monthIndex = newDate.getMonth();
-                var year = newDate.getFullYear();
-              
-                return monthNames[monthIndex] + ' ' + day + ', ' + year;
-              }
-              
-              console.log(formatDate(new Date()));
+              console.log(formatDate(post.pubDate));
               return (
                 <div className="carousel-card">
                   <div className="subcard">
                     <div className="heading">
                       <h4>{post.title}</h4>
-                      <p>{formatDate(new Date())}</p>
+                      <p>{formatDate(post.pubDate)}</p>
                     </div>
                     <div className="card-content">
                       <p>{StrippedText}</p>
