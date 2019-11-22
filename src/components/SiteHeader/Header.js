6035672bc4d8e5b62b22f94cf1c904ee186359ff
alka95 <deepujha1112@gmail.com>
2019-11-22 15:07:26 +0530

    navigation menu animation and responsive

--- src/components/SiteHeader/Header.js
+++ src/components/SiteHeader/Header.js
@@ -2,29 +2,50 @@ import React from "react"
 import { Link } from "gatsby"
 // import 'bootstrap/dist/js/bootstrap.bundle.min'
 
-const Header = ({location}) => {
-    console.log()
-    let headerClass
-    if(location.pathname === "/developer/"){
-        headerClass = 'nav-development'
+class Header extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            menuOpen: false,
+        }
     }
-    else if(location.pathname === "/foundation/"){
-        headerClass = 'nav-foundation'
-    }
-
-    let imgsrc 
-    if(location.pathname === "/developer/"){
-        imgsrc =  '/images/home/fantom_name_blue.svg'
-    }
-    else if(location.pathname === "/foundation/"){
-        imgsrc =  '/images/home/fantom_name_blue.svg'
-    }
-    else {
-        imgsrc = '/images/home/fantom header logo white v3.png'
-    }
+    
+      toggle = () => {
+        this.setState({
+          menuOpen: !this.state.menuOpen,
+        })
+        // setMenuOpen(!menuOpen)
+      }
+        
+      onClick = () =>{
+        this.toggle()
+      }
+    render () {
+        const { menuOpen, visible } = this.state;
+        console.log();
+        const menuClass = menuOpen ? "openMenu" : ""
+        let headerClass
+        if(this.props.location.pathname === "/developer/"){
+            headerClass = 'nav-development'
+        }
+        else if(this.props.location.pathname === "/foundation/"){
+            headerClass = 'nav-foundation'
+        }
+    
+        let imgsrc 
+        if(this.props.location.pathname === "/developer/"){
+            imgsrc =  '/images/home/fantom_name_blue.svg'
+        }
+        else if(this.props.location.pathname === "/foundation/"){
+            imgsrc =  '/images/home/fantom_name_blue.svg'
+        }
+        else {
+            imgsrc = '/images/home/fantom header logo white v3.png'
+        }
+    
     return(
         <header className={`header ${headerClass}`}>
-            <div className="container">
+            <div className="container navigation">
                 <h1 className="logo"><Link to="#"><img src={imgsrc} alt=""/></Link></h1>
                 <ul className="main-nav">
                     <li><Link to="/about/">What is Fantom?</Link></li>
@@ -34,17 +55,20 @@ const Header = ({location}) => {
                 </ul>
             </div> 
             <div id="mySidenav" className="sidenav">
-                <button className="sideNav-button">
+                <button className= {`sideNav-button ${headerClass}`} onClick={() => this.toggle()}>
                     <span className="navbar-toggler-icon top-bar"></span>
                     <span className="navbar-toggler-icon middle-bar"></span>
                     <span className="navbar-toggler-icon bottom-bar"></span>
                 </button>
-                <div>
-                    <ul className="main-nav">
+                <div className={`slide-right ${menuClass}` }>
+                    <span className="nav-img"><Link to="#"><img src="/images/home/fantom_name_blue.svg" alt=""/></Link></span>
+                    <span className="close-button" onClick={() => this.toggle()}>X</span>
+                    <ul>
                         <li><Link to="/about/">What is Fantom?</Link></li>
                         <li><Link to="/developer/">Developers</Link></li>
                         <li><Link to="#">Articles</Link></li>
                         <li><Link to="/foundation/">Foundation</Link></li>
+                        <li><a href="/Documents/wp_fantom_v1.6.pdf" target="_blank">White Paper</a></li>
                     </ul>
                 </div>
             </div>
@@ -79,7 +103,8 @@ const Header = ({location}) => {
                 </div>
             </nav> */}
         </header>   
-    )
+        )    
+    }
 }
 
 export default Header
