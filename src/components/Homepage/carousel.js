010da2bfd40f5b7a3f65235cd2830154b0fbf85a
alka95 <deepujha1112@gmail.com>
2019-11-24 22:06:09 +0530

    slider responsive

--- src/components/Homepage/carousel.js
+++ src/components/Homepage/carousel.js
@@ -2,6 +2,7 @@ import React from "react"
 import Slider from "react-slick"
 import axios from "axios"
 import AOS from 'aos'
+// const AOS = require('aos');
 import 'aos/dist/aos.css'
 
 function formatDate(newDate) {
@@ -23,14 +24,6 @@ class Carousel extends React.Component {
       posts: []
     }
   }
-  componentDidMount() {
-    const AOS = require('aos');
-    this.aos = AOS
-    this.aos.init({
-      once: true,
-      disable: 'mobile',
-  })
-}
 
   componentDidMount() {
     const data = 'https://medium.com/feed/fantomfoundation';
@@ -40,20 +33,26 @@ class Carousel extends React.Component {
         posts: fetchedPosts
       });
       console.log('posts', fetchedPosts)
-      console.log('post.link');
+    });
+    console.log('AOS');
+    console.log(AOS);
+    this.aos = AOS
+    this.aos.init({
+      once: true,
+      disable: 'mobile',
     })
   }
   render() {
     let postsState = this.state.posts
     console.log('postsState ', postsState);
     console.log('postsState type', typeof postsState);
-    var settings = {
+    const settings = {
       arrows: true,
       infinite: false,
       speed: 100,
       pauseOnHover: false,
-      slidesToShow: 4,
-      slidesToScroll: 1,
+      // slidesToShow: 4,
+      // slidesToScroll: 1,
       responsive: [
         {
           breakpoint: 1920,
@@ -61,25 +60,29 @@ class Carousel extends React.Component {
             slidesToShow: 3,
             slidesToScroll: 1,
           }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          
-        }
-      },
-        {
-            breakpoint: 769,
-            settings:{
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              dots: true,
-              arrows: false,
-            }
         },
         {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            
+          }
+        },{
+          breakpoint: 992,
+          settings:{
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },{
+        breakpoint: 769,
+          settings:{
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+          }
+        },{
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
@@ -88,38 +91,39 @@ class Carousel extends React.Component {
             arrows: false,
           }
         }
-    ],
+      ],
       autoplay: true,
       autoplaySpeed: 5000,
     };
-   
+    // AOS.refresh();
     return (
       <div className="inner-container carousel-section section">
         <h2 className="slider-heading" data-aos="fade-right">Latest articles</h2>
-        <Slider {...settings}>
-          {
-            postsState.map((post, index) => {
-              var StrippedString = post.content.split(" ").splice(0, 20).join(" ");
-              var StrippedText = StrippedString.replace(/(<([^>]+)>)/ig,"");
-              console.log(formatDate(post.pubDate));
-              return (
-                <div className="carousel-card" data-aos="fade-left" data-index={index} key={index}>
-                  <div className="subcard">
-                    <div className="heading">
-                      <h4>{post.title}</h4>
-                      <p>{formatDate(post.pubDate)}</p>
+        <div data-aos="fade-left" data-aos-duration="2000">
+          <Slider {...settings}>
+            {
+              postsState.map((post, index) => {
+                var StrippedString = post.content.split(" ").splice(0, 20).join(" ");
+                var StrippedText = StrippedString.replace(/(<([^>]+)>)/ig,"");
+                console.log(formatDate(post.pubDate));
+                return (
+                  <div className="carousel-card" data-index={index} key={index}>
+                    <div className="subcard">
+                      <div className="heading">
+                        <h4>{post.title}</h4>
+                        <p>{formatDate(post.pubDate)}</p>
+                      </div>
+                      <div className="card-content">
+                        <p>{StrippedText}</p>
+                      </div>
+                      <div className="read"><a href={post.link} target="_blank">Read More</a></div>
                     </div>
-                    <div className="card-content">
-                      <p>{StrippedText}</p>
-                    </div>
-                    <div className="read"><a href={post.link} target="_blank">Read More</a></div>
                   </div>
-                </div>
-              )
-            })
-          }
-          
+                )
+              })
+            }
           </Slider>
+        </div>
         </div>
       )
     }
