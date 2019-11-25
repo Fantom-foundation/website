e44f7221ecaf6b4f339ea20691ba4a2db5832f00
alka95 <deepujha1112@gmail.com>
2019-11-25 15:27:26 +0530

    update card animation

--- src/components/Homepage/Subtitle.js
+++ src/components/Homepage/Subtitle.js
@@ -15,8 +15,8 @@ class Subtitle extends React.Component {
         return(
             <div className="container section">
                 <div className="subtitle-section">
-                    <h2 data-aos="slide-up">The most advanced, fast, and secure consensus for distributed networks.</h2>
-                    <p data-aos="slide-up">Whether you’re issuing a stablecoin, creating a new smart economy, or powering a smart city, Fantom’s modular approach lets you focus on building the perfect decentralized application, or mainnet — as we call it.
+                    <h2 data-aos="fade-up" data-aos-easing="ease-in-cubic" data-aos-duration="1000" data-aos-offset="200">The most advanced, fast, and secure consensus for distributed networks.</h2>
+                    <p data-aos="fade-up" data-aos-easing="ease-in-cubic" data-aos-duration="1000" data-aos-offset="200">Whether you’re issuing a stablecoin, creating a new smart economy, or powering a smart city, Fantom’s modular approach lets you focus on building the perfect decentralized application, or mainnet — as we call it.
                     </p>
                 </div>
             </div>
