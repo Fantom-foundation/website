d58874cb8f77fe6bd6e9e890f1ebf898b7cfa3e3
alka95 <deepujha1112@gmail.com>
2019-11-26 16:19:17 +0530

    footer responsive fixed

--- src/components/SiteFooter/Footer.js
+++ src/components/SiteFooter/Footer.js
@@ -5,45 +5,39 @@ const Footer = () => {
         <footer>
             <div className="footer-section">
                 <div className="container footer-inner-container">
-                    <div className="footer-fantom-icon">
-                        <ul>
-                            <li><a href="#"><img src="/images/footer/fantom-logo-white-v-3.svg" alt="fantom icon" /></a></li>
-                            <li></li>
-                            <li>
+                    <div className="first-section">
+                        <div className="col first-col">
+                            <div className="footer-logo"> 
+                                <a href="#">
+                                    <img src="/images/footer/fantom-logo-white-v-3.svg" alt="fantom icon" />
+                                </a>
+                            </div>
+                            <div>
                                 <span><a target="_blank" href="https://twitter.com/FantomFDN"><img src="/images/footer/twitter.svg" alt="twitter" /></a></span>
                                 <span><a target="_blank" href="https://t.me/Fantom_English"><img src="/images/footer/telegram.svg" alt="telegram" /></a></span>
                                 <span><a target="_blank" href="https://github.com/Fantom-Foundation"><img src="/images/footer/github.svg" alt="github" /></a></span>
                                 <span><a target="_blank" href="#"><img src="/images/footer/reddit.svg" alt="reddit" /></a></span>
                                 <span><a target="_blank" href="https://www.linkedin.com/company/fantom-foundation/"><img src="/images/footer/linkedin.svg" alt="linkedin" /></a></span>
                                 <span><a target="_blank" href="https://medium.com/fantomfoundation"><img src="/images/footer/medium.svg" alt="medium" /></a></span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="footer-link">
-                        <div className="footer-first-col">
-                            <ul>
-                                <li><a href="/about/">What is Fantom?</a></li>
-                                <li><a href="/developer/">Developers</a></li>
-                                <li><a href="https://medium.com/fantomfoundation" target="_blank">Articles</a></li>
-                            </ul>
+                            </div>
                         </div>
-                        <div className="footer-first-col">
-                            <ul>
-                                <li><a href="/foundation/">Foundation</a></li>
-                                <li><a href="/Documents/wp_fantom_v1.6.pdf" target="_blank">White Paper</a></li>
-                                <li className="careers-link-hide"><a href="#">Careers</a></li>
-                            </ul>
+                        <div className="col second-col">
+                            <div><a href="/about/">What is Fantom?</a></div>
+                            <div><a href="/developer/">Developers</a></div>
+                            <div><a href="https://medium.com/fantomfoundation" target="_blank">Articles</a></div>
+                        </div>
+                        <div className="col third-col">
+                            <div><a href="/foundation/">Foundation</a></div>
+                            <div><a href="/Documents/wp_fantom_v1.6.pdf" target="_blank">White Paper</a></div>
                         </div>
                     </div>
-                    <div className="footer-small-section">
-                        <div className="footer-left">
-                            <p>@2019 Fantom Foundation</p>
-                        </div>
-                        <div className="footer-right">
-                            <p> <a href="/privay-policy/">Privacy Policy</a></p>
-                        </div>
+                    <div className="second-section">
+                        <div>@2019 Fantom Foundation</div>
+                        <div></div>
+                        <div className="privacy-policy"><a href="/privay-policy/">Privacy Policy</a></div>
                     </div>
                 </div>
+                
                 <div className="container footer-inner-container-mobile section">
                     <div className="footer-first-col">
                         <p className="footer-link"><a href="#"><img src="/images/footer/fantom new logo.svg" alt="fantom icon" /></a></p>
